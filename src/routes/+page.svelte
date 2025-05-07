49be15b58fec9091892a8c4741a6f1d3a02163c0
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-05-07 20:26:54 +0900

    feat: fullscreen modal on mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,45 @@
 <script lang="ts">
+	let dialog: HTMLDialogElement;
 	let isOpen = $state(false);
+
+	const open = () => {
+		isOpen = true;
+		if (window.innerWidth < 640) {
+			try {
+				dialog.showModal();
+			} catch {
+				dialog.close();
+				dialog.showModal();
+			}
+		} else {
+			try {
+				if (dialog.open) {
+					dialog.showModal();
+					dialog.close();
+				}
+				// eslint-disable-next-line no-empty
+			} catch {}
+			dialog.show();
+		}
+	};
+
+	const close = () => {
+		isOpen = false;
+		dialog.close();
+	};
 </script>
 
-<div class="fixed inset-6 flex flex-col-reverse items-end gap-y-4 sm:inset-8">
+<svelte:window
+	onresize={() => {
+		if (isOpen) open();
+	}}
+/>
+
+<div class="fixed inset-6 flex flex-col-reverse items-end gap-y-4">
 	<button
 		type="button"
 		aria-label={!isOpen ? 'Open' : 'Close'}
-		onclick={() => (isOpen = !isOpen)}
+		onclick={!isOpen ? open : close}
 		class="flex size-14 items-center justify-center rounded-3xl bg-indigo-700 text-white shadow-2xl"
 	>
 		<svg
@@ -20,27 +53,26 @@
 			<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 		</svg>
 	</button>
-	<!-- TODO Convert this to modal dialog on mobile. -->
-	<div
-		class={[
-			!isOpen && 'hidden',
-			'w-full overflow-y-auto shadow-2xl',
-			'max-sm:fixed max-sm:inset-0',
-			'sm:max-h-160 sm:w-96 sm:flex-1 sm:rounded-3xl',
-			'flex flex-col gap-y-8 bg-gray-200 *:bg-white *:p-4',
-		]}
-	>
-		<header class="sticky top-0 flex h-16 shrink-0 items-center justify-end sm:hidden">
-			<button type="button" onclick={() => (isOpen = false)}>Close</button>
-		</header>
-		{#each { length: 10 }}
-			<article class="aspect-square">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit.
-			</article>
-		{/each}
-		<footer class="flex h-24 shrink-0 items-center justify-center">End of Document</footer>
-	</div>
 </div>
+
+<dialog
+	bind:this={dialog}
+	oncancel={close}
+	class={[
+		'max-h-full max-w-full overflow-y-auto max-sm:w-full sm:right-6 sm:bottom-26 sm:ml-auto sm:h-160 sm:max-h-[calc(100%-var(--spacing)*32)] sm:w-96 sm:rounded-3xl sm:shadow-2xl',
+		'flex-col gap-y-8 bg-gray-200 *:bg-white *:p-4 open:flex',
+	]}
+>
+	<header class="sticky top-0 flex h-16 shrink-0 items-center justify-end sm:hidden">
+		<button type="button" onclick={close}>Close</button>
+	</header>
+	{#each { length: 10 }}
+		<article class="aspect-square">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit.
+		</article>
+	{/each}
+	<footer class="flex h-24 shrink-0 items-center justify-center">End of Document</footer>
+</dialog>
 
 <style>
 	header {
@@ -48,5 +80,41 @@
 			0px 2px 4px -3px rgba(0, 0, 0, 0.2),
 			0px 4px 5px -2px rgba(0, 0, 0, 0.14),
 			0px 1px 10px -10px rgba(0, 0, 0, 0.12);
+	}
+
+	dialog[open] {
+		translate: 0 0;
+	}
+
+	dialog[open],
+	dialog[open]::backdrop {
+		opacity: 1;
+	}
+
+	@starting-style {
+		dialog[open] {
+			translate: 0 2rem;
+		}
+
+		dialog[open],
+		dialog[open]::backdrop {
+			opacity: 0;
+		}
+	}
+
+	dialog,
+	dialog::backdrop {
+		opacity: 0;
+		transition-duration: 0.5s;
+		transition-property: opacity, translate;
+		transition-timing-function: cubic-bezier(0.34, 1.56, 0.64, 1);
+	}
+
+	@supports (transition-behavior: allow-discrete) and (overlay: auto) {
+		dialog,
+		dialog::backdrop {
+			transition-behavior: allow-discrete;
+			transition-property: opacity, translate, display, overlay;
+		}
 	}
 </style>
