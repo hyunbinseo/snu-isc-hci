8071b86a609a3f7c71dea4506d23847ce8bdb6df
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-05-15 13:16:10 +0900

    fix: mount app in shadow root

--- src/entrypoints/content.ts
+++ src/entrypoints/content.ts
@@ -1,10 +1,19 @@
-import { mount } from 'svelte';
+import { mount, unmount } from 'svelte';
 import App from '../lib/App.svelte';
 
 export default defineContentScript({
 	matches: import.meta.env.DEV ? ['<all_urls>'] : ['https://myetl.snu.ac.kr/*'],
-	runAt: 'document_end',
-	main() {
-		mount(App, { target: document.body });
+	cssInjectionMode: 'ui',
+	main: async (ctx) => {
+		let app: Record<string, any>;
+		createShadowRootUi(ctx, {
+			name: 'news-widget',
+			position: 'overlay',
+			anchor: 'body',
+			onMount: (uiContainer) => {
+				app = mount(App, { target: uiContainer });
+			},
+			onRemove: () => unmount(app),
+		}).then((ui) => ui.mount());
 	},
 });
