1105d01874a1d853da380e411873d251a4e7b3e3
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-05-19 09:44:21 +0900

    fix: firefox android install from file

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'wxt';
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+	manifest: {
+		name: '쓸모있조',
+		browser_specific_settings: {
+			gecko: {
+				id: '{dbe837d3-dd6a-4ef2-87f6-26eb48dd6ceb}', // Random UUID v4
+			},
+		},
+	},
 	srcDir: 'src',
 	modules: ['@wxt-dev/module-svelte'],
 	vite: () => ({ plugins: [tailwindcss()] }),
